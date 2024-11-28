6cfa38737f6de6b6b0f732315e68177ee02f7dec
andreacb1995 <andrea.carballido.ballesteros@gmail.com>
2024-11-28 16:01:40 +0100

    Primer commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.getElementById("formulario-login").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evitar que se recargue la página
+  mensajeError.style.display = "none"
+  const usuario = document.getElementById("usuario").value.trim();
+  const clave = document.getElementById("clave").value.trim();
+  const mensajeError = document.getElementById("mensaje-error");
+  
+  mensajeError.textContent = "";
+
+  //console.log(usuario);
+  //console.log(clave);
+
+  // Enviar datos al backend
+  fetch('http://localhost/proyectofinal1/login.php', {       
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ usuario, clave })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.message && data.message.trim().toLowerCase() === "inicio de sesión exitoso".toLowerCase()) {
+      // redirigir al usuario a la página de bienvenida
+      window.location.href = 'bienvenido.html';
+    } else {
+      mensajeError.textContent = data.message;
+      mensajeError.style.display = "block";
+    }
+  })
+  .catch(error => {
+    console.error("Error al iniciar sesión:", error);
+    mensajeError.textContent = "Ocurrió un error al intentar iniciar sesión.";
+    mensajeError.style.display = "block";
+  });
+});
